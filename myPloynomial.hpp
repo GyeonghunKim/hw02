993a8390199c2c5b060763fef3c3c051d60feef1
GHKim <aass98998@snu.ac.kr>
2018-10-02 14:30:38 +0900

    Initial commit

---
+++ myPloynomial.hpp
@@ -0,0 +1,10 @@
+//
+// Created by ghkim on 18. 10. 2.
+//
+
+#pragma once
+
+#include <iostream>
+
+template <typename Container>
+auto
---
+++ myPolynomialTest.hpp
@@ -0,0 +1,6 @@
+//
+// Created by ghkim on 18. 10. 2.
+//
+
+# pragma once
+
